2163b52f87fe8c3390e004c2d906af1f7db26cff
Tle <title1337@gmail.com>
2026-03-15 09:50:04 +0700

    feat ex3

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,13 @@
 // Start coding here
+let addnumber = (a,b) => {
+    return a + b;
+}
+let subtractnumber = (a,b) => {
+    return a - b;
+}   
+let multiplynumber = (a,b) => {
+    return a*b;
+}   
+let dividenumber = (a,b) => {
+    return a/b;
+}
