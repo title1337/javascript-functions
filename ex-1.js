7ae3e57807308f621f2bda03fd6ef7be3d4b0819
Tle <title1337@gmail.com>
2026-03-15 09:42:37 +0700

    feat ex1

--- ex-1.js
+++ ex-1.js
@@ -1,1 +1,19 @@
+const { Result } = require("pg");
+
 // Start coding here
+function add(a,b) {
+    Result = a + b;
+    return Result;
+}
+function subtract(a,b) {
+    Result = a - b;
+    return Result;
+}   
+function multiply(a,b) {
+    result = a * b;
+    return result;
+}
+function divide(a,b) {
+    result = a/b;
+    return result;
+}
