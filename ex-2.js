77bf3bb66bce7402691d1fc0893606eac40cd555
Tle <title1337@gmail.com>
2026-03-15 09:48:09 +0700

    feat ex2

--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,13 @@
 // Start coding here
+let addNumer = function(a,b) {
+    return a + b;
+}
+let subtractNumber = function(a,b) {
+    resturn a - b;
+}
+let multiplyNumber = function(a,b) {
+    resturn a * b;
+}   
+let divideNumber = function(a,b) {
+    resturn a / b;
+}   
