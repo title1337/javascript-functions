602b6e121097b0f045d3acf6223378b3f85f9e30
Tle <title1337@gmail.com>
2026-03-15 09:58:04 +0700

    feat ex4

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,19 @@
 // Start coding here
-let calculator = {};
+let calculator = {
+    add : function add(a,b) {
+        return a + b;
+    },
+    subtract : function subtract(a,b) {
+        return a - b;
+    },
+    multiply : function multiply(a,b) {
+        return a * b;
+    },
+    divide : function divide(a,b) {
+        return a / b;
+    }
+};
+let addResult = calculator.add(10,20);
+console.log(addResult);
+let divideResult = calculator.divide(3000,10);
+console.log(divideResult);
